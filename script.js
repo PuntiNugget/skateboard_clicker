4b3576e2e0b685ab9059b2b4a805fafd5f5541da
PuntiNugget <pbura@students.wcpss.net>
2026-02-12 08:18:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-const OlliButton = document.getElementById('Ollie');
-const output = document.getElementById('output');
+let count = 0;
 
-OlliButton.addEventListener('Click', function(event) {
-    const ClickTYpe = event.button === 0 ? "Left Click" :
-      event.button === 1 ? "Middle Click" :
-      event.button === 2 ? "Right Click" : 'Unknown-Click';
-
-  const displayMessage = 'Type: ${ClickType}, coordinates {${event.clientX}, ${event.clientY}}';
-
-  output.textContent = displayMessage;
-
-  console.log('A click occurred:', event);
-});
+const ClickButton = document.getElementById('
